8e309bc4b9cc2cf77652d42c761df988ab9e31e0
Daeyun <sweet_ray@naver.com>
2024-06-23 16:32:35 +0900

    스크롤뷰 사용해보기

---
+++ ScrollViewPractice/ScrollViewPractice/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  ScrollViewPractice
+//
+//  Created by 권대윤 on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var scrollView = UIScrollView()
+    
+    private var stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 16
+        return stack
+    }()
+    
+    private let rectHeight: CGFloat = 200
+    private let numberOfRects = 20
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 1..<numberOfRects {
+            let rect = createRectangle()
+            stackView.addArrangedSubview(rect)
+        }
+        
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        
+        scrollView.addSubview(stackView)
+        stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        
+        scrollView.contentSize.height = (rectHeight * CGFloat(numberOfRects)) + 100
+        
+        scrollView.backgroundColor = .systemYellow
+        scrollView.delegate = self
+    }
+    
+    private func createRectangle() -> UIView {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: rectHeight).isActive = true
+        view.widthAnchor.constraint(equalToConstant: rectHeight).isActive = true
+        view.backgroundColor = .systemGray
+        view.layer.cornerRadius = 16
+        return view
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        print("DEBUG: Scroll offset \(scrollView.contentOffset)")
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print(#function)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        print(#function)
+    }
+}
+
+#Preview {
+    ViewController()
+}
+
